cc17eab0053a62568bc67eb69a09aa3d5a3e6259
Adari Sri Venkata Anjani Sai <SYR00407@syrencloud.com>
2023-05-19 10:56:33 +0530

    studentsdetails completed

---
+++ exam table creation.sql
@@ -0,0 +1,7 @@
+CREATE TABLE gvp.exams(
+	roll int,
+    name varchar(255),
+    branch varchar(255),
+    marks int,
+    grade varchar(255)
+    );
---
+++ studentdetails table creation.sql
@@ -0,0 +1,7 @@
+CREATE TABLE studentdetails(
+    roll int,
+    name varchar(255),
+    gender varchar(255),
+    branch varchar(255),
+    city varchar(255),
+    country varchar(255));
