0facb2581136e8fa3c09f311eb0a674989dc3b2e
asvaskamal403 <133895059+asvaskamal403@users.noreply.github.com>
2023-05-29 00:38:39 +0530

    Delete courses table creation.sql

--- courses table creation.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE  courses ;
-CREATE TABLE courses (
-     courseID varchar(255) NOT NULL UNIQUE,
-     offeredby varchar(255) NOT NULL,
-     coursename varchar(255),
-     durationinyrs int NOT NULL,
-     cost int NOT NULL
-);
