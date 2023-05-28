afc665a3c95fb214faf8ea0a7f91992cc909f3d7
asvaskamal403 <133895059+asvaskamal403@users.noreply.github.com>
2023-05-29 00:42:07 +0530

    Delete studentdetails table creation.sql

--- studentdetails table creation.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE studentdetails;
-CREATE TABLE studentdetails(
-    roll int PRIMARY KEY UNIQUE NOT NULL,
-    name varchar(255) NOT NULL,
-    branch varchar(255) NOT NULL,
-    city varchar(255) NOT NULL,
-    country varchar(255) NOT NULL);
